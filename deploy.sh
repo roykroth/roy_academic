ab1c5e929304e490d19fee96fa722a357c9a910e
Roy Roth <roykroth@gmail.com>
2017-09-12 20:42:08 -0700

    test change

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,6 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
-# Go To Public folder
-cd public
-# Add changes to git.
 git add -A
 
 # Commit changes.
@@ -19,6 +16,3 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-
-# Come Back
-cd ..
