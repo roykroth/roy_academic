c8b40afb54f0f121493a131648565315340f8a83
Roy Roth <roykroth@gmail.com>
2018-11-01 14:20:42 -0700

    New workflow

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,12 @@
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Build the project.
-hugo # if using a theme, replace by `hugo -t <yourtheme>`
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-git add -A
+# Go To Public folder
+cd public
+# Add changes to git.
+git add .
 
 # Commit changes.
 msg="rebuilding site `date`"
@@ -16,3 +19,6 @@ git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
+
+# Come Back up to the Project Root
+cd ..
